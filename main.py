eda3a043eb6f3e8a744fa21b5146d90ab2ff108f
Mark Barnett <>
2021-01-07 21:19:08 +0000

    azure authentication now working.

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
+from auth import CREDENTIALS
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/pages/")
 
+numbers = [
+    1,
+    2,
+    3,
+]
 
-numbers = [1,2,3,4,6,6,7,8]
 
-@app.get('/')
+@app.get("/")
 def home_get(request: Request):
-    return templates.TemplateResponse('index.html', {"request": request, "numbers": numbers}) # request must be passed
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "id": CREDENTIALS}
+    )  # request must be passed
+
 
 @app.get("/dashboard")
 def dashboard_get(request: Request):
-    return templates.TemplateResponse('dashboard.html', {"request": request, "numbers": numbers}) # request must be passed
-
+    return templates.TemplateResponse(
+        "dashboard.html", {"request": request, "numbers": numbers}
+    )  # request must be passed
