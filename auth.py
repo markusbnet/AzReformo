86a5b0138af7fbb4fb2789b4c198eff8a174ee66
hbates1 <Harley.Bates@kpmg.co.uk>
2021-01-08 13:15:07 +0000

    normal auth

--- auth.py
+++ auth.py
@@ -8,4 +8,7 @@ client_id = os.getenv("CLIENT_ID", "NA")
 client_secret = os.getenv("CLIENT_SECRET", "NA")
 tenant_id = os.getenv("TENANT_ID", "NA")
 
-CREDENTIALS = ClientSecretCredential(tenant_id, client_id, client_secret,)
+CREDENTIALS = ServicePrincipalCredentials(
+    client_id=client_id, secret=client_secret, tenant=tenant_id,
+)
+
