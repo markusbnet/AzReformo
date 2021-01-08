817ad24f7e2f1290e595c1f3e8f0f60164f42a3c
hbates1 <Harley.Bates@kpmg.co.uk>
2021-01-08 09:27:47 +0000

    using azure identity, common credentials is being deprecated

--- auth.py
+++ auth.py
@@ -3,11 +3,13 @@ import os
 # Azure stuff
 from azure.common.credentials import ServicePrincipalCredentials
 
+from azure.identity import ClientSecretCredential
+
 # Get environment variables
 client_id = os.getenv("CLIENT_ID", "NA")
 client_secret = os.getenv("CLIENT_SECRET", "NA")
 tenant_id = os.getenv("TENANT_ID", "NA")
 
-CREDENTIALS = ServicePrincipalCredentials(
-    client_id=client_id, secret=client_secret, tenant=tenant_id,
+CREDENTIALS = ClientSecretCredential(
+    tenant_id, client_id, client_secret,
 )
