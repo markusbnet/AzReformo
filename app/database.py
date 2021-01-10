b3e3c1a11ebd8d35b648dd8fe6d4101afd4d9015
markusbnet <markbarnett3@gmail.com>
2021-01-10 22:36:55 +0000

    basicc database config

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import settings
+
+
+DATABASE_URL = f"postgresql://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_url}:5432/{settings.postgres_db}"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
