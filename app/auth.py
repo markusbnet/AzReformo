f7d1bf564e364f70ceb9d5f1b62aba2af16669c4
markusbnet <markbarnett3@gmail.com>
2021-01-10 22:35:59 +0000

    updating how config works

--- app/auth.py
+++ app/auth.py
@@ -5,5 +5,7 @@ from azure.common.credentials import ServicePrincipalCredentials
 from config import settings
 
 CREDENTIALS = ServicePrincipalCredentials(
-    client_id=settings.client_id, secret=settings.secret_id, tenant=settings.tenant_id,
+    client_id=settings.client_id,
+    secret=settings.client_secret,
+    tenant=settings.tenant_id,
 )
--- app/config.py
+++ app/config.py
@@ -4,16 +4,17 @@ import os
 
 class Settings(BaseSettings):
     # Azure
-    client_id: str = os.getenv("CLIENT_ID", "NA")
-    secret_id: str = os.getenv("CLIENT_SECRET", "NA")
-    tenant_id: str = os.getenv("TENANT_ID", "NA")
+    client_id: str
+    client_secret: str
+    tenant_id: str 
 
     # Postgres
     postgres_url: str = os.getenv("POSTGRES_URL", "localhost")
-    postgres_port: str = os.getenv("POSTGRES_PORT", "5432")
     postgres_user: str = os.getenv("POSTGRES_USER", "postgres")
-    postgres_password = os.getenv("POSTGRES_PASSWORD", "NA")
-    postgres_db = os.getenv("POSTGRES_DB", "azreformo")
+    postgres_password: str
+    postgres_db: str  = os.getenv("POSTGRES_DB", "azreformo")
 
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
