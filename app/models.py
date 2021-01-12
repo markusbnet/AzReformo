8406faea2160d316577226833c47c5de1d63a5f1
markusbnet <markbarnett3@gmail.com>
2021-01-12 21:04:25 +0000

    updated model for storageaccounts

--- app/models.py
+++ app/models.py
@@ -1,14 +1,15 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.sqltypes import Boolean
 from sqlalchemy.types import Date
 from database import Base
 
 
-class Record(Base):
-    __tablename__ = "Records"
+class StorageAcounts(Base):
+    __tablename__ = "StorageAccount"
 
     id = Column(Integer, primary_key=True, index=True)
-    date = Column(Date)
-    country = Column(String(255), index=True)
-    cases = Column(Integer)
-    deaths = Column(Integer)
-    recoveries = Column(Integer)
+    name = Column(String(255), index=True)
+    public = Column(Boolean)
+    tls = Column(String(255), index=True)
+    https = Column(String(255), index=True)
+
